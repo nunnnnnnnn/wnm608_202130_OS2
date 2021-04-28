fd931492c535907f70067e1d58d399c4a388764f
Lucille Huang <robin@Xuelais-MBP.attlocal.net>
2021-04-28 16:19:09 -0700

    upload php and data

---
+++ lucille.huang/php/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+function pretty_dump($data) {
+	echo "<pre>",var_dump($data),"</pre>";
+}
+
+
+function file_get_json($filename) {
+	$data_string = fiile_get_contents($filename);
+	return json_decode($data_string);
+}
+
+/* DATABASE CONNECTION */
+functioin MYSQLConn() {
+	include_once "data/auth.php";
+
+	@$conn = new mysqli(...MYSQLIAuth());
+	if($conn->connect_errno) 
+		die($conn->connect_error);
+
+	$conn->set_charset('utf8');
+
+	return $conn;
+}
+
+
+/* DATABASE CALL */
+function MYSQLIQuery($sql) {
+	$conn = MYSQLIConn();
+
+
+}
